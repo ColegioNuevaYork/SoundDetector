ed4f98d23cbe5f4cc7482c89b97c9793128b6f54
Andres Morales <mora200217@hotmail.com>
2017-02-25 12:07:39 -0500

    Configuración Arduino Inicial

---
+++ SoundDetection/SoundDetection.ino
@@ -0,0 +1,21 @@
+/* 
+ *  Código creado por: Andrés Mauricio Morales Martínez 
+ *  el 25-02-2017. Algoritmo principal, de detección 
+ *  de ondas sonoras ambientales, por medio de sensor de 
+ *  ruido "Groove", por medio de entradas análogas que 
+ *  posteriormente serán exportadas alambiente processing 
+ *  por medio de comunicación serial, con el fin de 
+ *  representar graficamente lo valores de entrada
+ */
+
+
+void setup() {
+  //  Algoritmo inicial de configuración general del proyecto 
+
+}
+
+
+void loop() {
+  // Algoritmo de repetición general
+
+}
